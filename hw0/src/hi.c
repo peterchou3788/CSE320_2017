e4120597988401d7e702a02c344e5c988de069f2
Peter Chou <peter.chou@stonybrook.edu>
2017-01-27 00:20:18 -0500

    added src/hi.c

--- hw0/src/hi.c
+++ hw0/src/hi.c
@@ -5,7 +5,9 @@
  * as you can see it simply returns the string "Hi" Or does it??
  */
 
+char hi[5] = "Hi";
 char* say_hi(){
-    return "Hello";
+	printf("%s\n",hi );
+    return hi;
 }
 /* Back over to main to finish our program */
