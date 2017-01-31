8ea417daa0e841e8ff9b04b2fb78e15361e3b2b2
Peter Chou <peter.chou@stonybrook.edu>
2017-01-31 15:23:16 -0500

    hw1

--- hw1/src/hw1.c
+++ hw1/src/hw1.c
@@ -4,9 +4,51 @@
 // DO NOT define a main function here!
 
 char validargs(int argc, char** argv, FILE** in, FILE** out) {
-	char ret = 0;
 
 	/* code here */
+	if(argc < 2 || argc > 6)
+		USAGE(EXIT_FAILURE);
 
-	return ret;
+	char* h = "-h";
+	char* s = "-s";
+	char* t = "-t";
+	char* e = "-e";
+	char* d = "-d";
+
+		char* arg1 = *(argv + 1);		//First Arg (h,s,t)
+		char* arg2 = *(argv + 2);		//Second Arg(e,d)
+		char* arg3 = *(argv + 3);		//Third Arg(input file)
+		char* arg4 = *(argv + 4);		//Fourth Arg(output file)
+
+		if(arg1 == s)
+		{
+			if(arg2 == e || arg2 == d)
+			{
+				*in = fopen(arg3,"r");
+				*out = fopen(arg4,"w");
+				USAGE(EXIT_SUCCESS);
+			}
+		}
+
+		if(arg1 == t)
+		{
+			if(arg2 == e || arg2 == d)
+			{
+				*in = fopen(arg3,"r");
+				*out = fopen(arg4,"w");
+				USAGE(EXIT_SUCCESS);
+			}
+		}
+
+		if(arg1 == h)
+		{
+			USAGE(EXIT_SUCCESS);
+		}
+
+		else
+			USAGE(EXIT_FAILURE);
+
 }
+
+
+
